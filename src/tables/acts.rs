59d0d3c31c4642f39cbfc5fa7b37200a3490eb30
Graham Forest <graham.forest@protonmail.com>
2025-03-31 20:16:57 -0700

    Add some more types for examples

--- src/tables/acts.rs
+++ src/tables/acts.rs
@@ -21,9 +21,24 @@ pub static TABLE_Acts: LazyLock<Vec<ActsRow>> = LazyLock::new(|| {
             r#id: df
                 .string_from_offset(row.get(0..8).unwrap().get_i32_le() as usize)
                 .unwrap(),
-            r#part: row.get(8..16).unwrap().get_i32_le(),
+            r#ui_title: row.get(8..16).unwrap().get_i32_le(),
             r#act_number: row.get(16..20).unwrap().get_i32_le(),
             r#is_end_game: row.get(40).unwrap().to_le() != 0,
+            r#unknown_int: row.get(41..43).unwrap().get_i16_le(),
+
+            r#unknown_foreign_array: df
+                .array_from_offset(
+                    row.get(53..59).unwrap().get_i32_le() as usize,
+                    row.get(45..51).unwrap().get_i32_le() as usize,
+                    16,
+                )
+                .unwrap()
+                .iter()
+                .map(|x| x.clone().get_i32_le())
+                .collect(),
+            r#description: df
+                .string_from_offset(row.get(125..131).unwrap().get_i32_le() as usize)
+                .unwrap(),
         })
         .collect()
 });
@@ -31,9 +46,12 @@ pub static TABLE_Acts: LazyLock<Vec<ActsRow>> = LazyLock::new(|| {
 #[derive(Debug)]
 pub struct ActsRow {
     pub r#id: String,
-    pub r#part: i32,
+    pub r#ui_title: i32,
     pub r#act_number: i32,
     pub r#is_end_game: bool,
+    pub r#unknown_int: i16,
+    pub r#unknown_foreign_array: Vec<i32>,
+    pub r#description: String,
 }
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, Ord, PartialOrd)]
